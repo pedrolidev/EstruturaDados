661db4d270cac67e08f6a6b56e2c45b682d46cb7
Pedro Oliveira <126485301+pedrolidev@users.noreply.github.com>
2024-02-20 14:33:56 -0300

    Add files via upload

---
+++ olaMundo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Comentário de linha
+/*
+Comentário de bloco
+ocupam várias linhas
+*/
+
+func main() {
+	fmt.Println("Olá, mundo!")
+	fmt.Println("Olá! Temos", 30,"alunos na turma de Estrutura de Dados.")
+}
