369974ac219624983124cdc9ce8ef0de5644858f
Pedro Oliveira <126485301+pedrolidev@users.noreply.github.com>
2024-02-20 14:35:00 -0300

    Add files via upload

---
+++ 01-variaveis/variaveis.go
@@ -0,0 +1,57 @@
+package main
+
+import "fmt"
+
+/*
+- Inteiros
+int8, int16, int32, int64
+uint8, uint16, uint32, uint64 -> unsigned
+int -> tipo genérico, ocupa 32 ou 64 bits, dependendo da arquitetura
+uint
+
+byte -> uint8
+rune -> int32 -> caractere Unicode
+
+- Ponto fLutuante
+float32 ou float64
+float64 -> padrão
+
+complex64 ou complex128
+
+- Texto
+string (cada caractere ocupa 8 bits de memória)
+
+- Booleanos
+bool -> true ou false
+
+- Outros tipos
+- nil
+- ponteiros
+
+*/
+
+var a int
+var b = 15
+// c := 10 ->  não posso fazer!
+const PI = 3.1415 // preciso fazer declaração implícita para constantes
+
+func main(){
+	fmtPrintln(12.4 + 43.171)
+	msg := 'texto
+em bloco'
+
+	fmt.Println(msg)
+
+	var x int
+	var msg1, msg2 string
+
+	var y = 14.5
+	z := 20
+
+	x = 18
+	msg1 = "oi"
+	msg2 = "olá"
+	fmt.Println(x, y, z)
+	fmt.Println(msg1, msg2)
+
+}
