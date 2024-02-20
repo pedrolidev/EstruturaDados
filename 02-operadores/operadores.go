2a6f098d30eba02be5e40bc1c410e5e095a4a7c3
Pedro Oliveira <126485301+pedrolidev@users.noreply.github.com>
2024-02-20 14:35:54 -0300

    Add files via upload

---
+++ 02-operadores/operadores.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x = 2
+	x := 4.5
+
+	//operadores aritméticos
+	fmt.Printf(x + y)
+	fmt.Printf(x - y)
+	fmt.Printf(x * y)
+	fmt.Printf(x / y)
+	fmt.Printf(x % y)
+
+	x += 3
+	// unários
+	x++ // -> x
+	y--
+
+	// Operadores de comparação
+	fmt.Printf(x > y)
+	fmt.Printf(x < y)
+	fmt.Printf(x >= y)
+	fmt.Printf(x <= y)
+	fmt.Printf(x == y)
+	fmt.Printf(x != y)
+
+	var a, b = true, false
+
+	//Operadores lógicos
+	
+}
